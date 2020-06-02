5325d1c21da5c86fe6fb16c5719d7bc3ee090eac
Tony Tom K <tony.tomk@thermofisher.com>
2020-06-03 01:32:06 +0530

    covid dashboard page

---
+++ pages/dashboard/precache-manifest.38eef0dcbc051b18ac5f1f7bc090a17f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbc493b8f3d6f974064",
+    "url": "css/app.23fd344b.css"
+  },
+  {
+    "revision": "3437fb5942be63317fc9",
+    "url": "css/chunk-vendors.29464375.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "57dc372341f8d51284b7b6146c06529f",
+    "url": "index.html"
+  },
+  {
+    "revision": "dae5548702f23a03f249",
+    "url": "js/about.bfdc7484.js"
+  },
+  {
+    "revision": "0dbc493b8f3d6f974064",
+    "url": "js/app.542a3d9c.js"
+  },
+  {
+    "revision": "3437fb5942be63317fc9",
+    "url": "js/chunk-vendors.a582e681.js"
+  },
+  {
+    "revision": "8d7a60377facc8fac6352db3e4010514",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
